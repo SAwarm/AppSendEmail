e1eb209f20a4883f4fdfd20d3d03c769ca19bfc4
jonas-elias <jonasdasilvaelias@gmail.com>
2022-06-30 21:24:19 -0300

    feat: config file - parameters mail

---
+++ src/Config/MailConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Src\Config;
+
+class MailConfig
+{
+    public static $service = 'smtp-mail.outlook.com';
+    public static $emailName = 'email@hotmail.com';
+    public static $emailPassword = 'password';
+    public static $port = 587;
+}
