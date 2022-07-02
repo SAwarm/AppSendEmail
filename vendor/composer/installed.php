c800daff2738b6f2318af8af429c0669ce1f5c59
jonas-elias <jonasdasilvaelias@gmail.com>
2022-07-01 21:05:50 -0300

    feat: composer install in the project

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '88542fa009589d54e3a76c86acea8fbe88ff645a',
+        'name' => 'jonas-elias/app-send-mail',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jonas-elias/app-send-mail' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '88542fa009589d54e3a76c86acea8fbe88ff645a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
