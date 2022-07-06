dc229af88276a7c323e272fdfe07fb0c9511ae97
jonas-elias <jonasdasilvaelias@gmail.com>
2022-07-06 20:17:29 -0300

    fix: extension files routes

--- src/Config/routes/routes.php
+++ src/Config/routes/routes.php
@@ -9,7 +9,7 @@ require_once __DIR__ . "./core/router.php";
 // Static GET
 // In the URL -> http://localhost
 // The output -> Index
-get('/', './../src/Views/index.php');
+get('/', './../src/App/Views/SendMail.phtml');
 
 post('/send', './../src/App/ProcessMail.php');
 
@@ -21,4 +21,4 @@ post('/send', './../src/App/ProcessMail.php');
 // For GET or POST
 // The 404.php which is inside the views folder will be called
 // The 404.php has access to $_GET and $_POST
-any('/404','views/404.php');
+any('/404', './../src/App/Views/404.phtml');
