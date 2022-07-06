4f4cf5bbb536ce449b123e30847ad54904462a49
jonas-elias <jonasdasilvaelias@gmail.com>
2022-07-05 22:58:02 -0300

    fix: attribute mail

--- src/Config/MailConfig.php
+++ src/Config/MailConfig.php
@@ -5,7 +5,7 @@ namespace Src\Config;
 class MailConfig
 {
     public static $service = 'smtp-mail.outlook.com';
-    public static $emailName = 'email@hotmail.com';
+    public static $emailName = 'mail@hotmail.com';
     public static $emailPassword = 'password';
     public static $port = 587;
 }
