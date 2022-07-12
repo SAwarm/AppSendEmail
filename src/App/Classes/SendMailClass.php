5d82cce948303dbd897601f14e3c04a14b77ff4a
jonas-elias <jonasdasilvaelias@gmail.com>
2022-07-11 23:42:25 -0300

    fix: sendMailClass

--- src/App/Classes/SendMailClass.php
+++ src/App/Classes/SendMailClass.php
@@ -20,31 +20,20 @@ class SendMailClass extends AttributesMailClass
         $mail = new PHPMailer(true);
 
         try {
-            //Server settings
-            //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                                    //Enable verbose debug output
-            $mail->isSMTP();                                                            //Send using SMTP
-            $mail->Host       = MailConfig::$service;                               //Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                                   //Enable SMTP authentication
+            $mail->isSMTP();                                                              //Send using SMTP
+            $mail->Host       = MailConfig::$service;                                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                                     //Enable SMTP authentication
             $mail->Username   = MailConfig::$emailName;                                   //SMTP username
-            $mail->Password   = MailConfig::$emailPassword;                                             //SMTP password
+            $mail->Password   = MailConfig::$emailPassword;                               //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-            //$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;                          //Enable implicit TLS encryption
-            $mail->Port       = MailConfig::$port;                                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            $mail->Port       = MailConfig::$port;                                        //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
             //Recipients
             $mail->setFrom(MailConfig::$emailName, 'Remetente estudo');
-            $mail->addAddress($this->to);                                               //Add a recipient
-            //$mail->addAddress('ellen@example.com');                                   //Name is optional
-            //$mail->addReplyTo('info@example.com', 'Information');
-            //$mail->addCC('cc@example.com');
-            //$mail->addBCC('bcc@example.com');
-
-            //Attachments
-            //$mail->addAttachment('/var/tmp/file.tar.gz');                             //Add attachments
-            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');                        //Optional name
+            $mail->addAddress($this->to);                                                 //Add a recipient
 
             //Content
-            $mail->isHTML(true);                                                        //Set email format to HTML
+            $mail->isHTML(true);                                                          //Set email format to HTML
             $mail->Subject =  $this->subject;
             $mail->Body    =  $this->message;
             $mail->AltBody = 'É necessário um cliente que suporte HTML5';
