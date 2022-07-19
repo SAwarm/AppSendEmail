07c6b05890ff783e45b5db57544cb20c793e135b
jonas-elias <jonasdasilvaelias@gmail.com>
2022-07-18 23:34:06 -0300

    comments: attributesMailClass

--- src/App/Classes/AttributesMailClass.php
+++ src/App/Classes/AttributesMailClass.php
@@ -2,22 +2,49 @@
 
 namespace Src\App\Classes;
 
+/**
+ * Class AttributesMailClass
+ * @package Src\App\Classes
+ */
 class AttributesMailClass
 {
+    /**
+     * @var string
+     */
     private $to;
 
+    /**
+     * @var string
+     */
     private $subject;
 
+    /**
+     * @var string
+     */
     private $message;
 
+    /**
+     * @var array
+     */
     public $status = array('status_code' => null, 'description_status' => null);
 
-    public function __get($atribute)
+    /**
+     * method __get elements
+     * @param string $attribute
+     * @return mixed
+     */
+    public function __get($attribute): mixed
     {
-        return $this->$atribute;
+        return $this->$attribute;
     }
 
-    public function __set($atribute, $value)
+    /**
+     * method __set elements
+     * @param string $attribute
+     * @param mixed $value
+     * @return void
+     */
+    public function __set($atribute, $value): void
     {
         $this->$atribute = $value;
     }
