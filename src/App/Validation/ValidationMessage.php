de6538884fd53bfb86b164c7c7a1f78a32a35a65
jonas-elias <jonasdasilvaelias@gmail.com>
2022-07-18 23:34:53 -0300

    comments: validationMessage

--- src/App/Validation/ValidationMessage.php
+++ src/App/Validation/ValidationMessage.php
@@ -4,14 +4,35 @@ namespace Src\App\Validation;
 
 use Src\App\Classes\AttributesMailClass;
 
+/**
+ * Class ValidationMessage
+ * @package Src\App\Validation
+ */
 class ValidationMessage extends AttributesMailClass
 {
+    /**
+     * to mail
+     * @var string
+     */
     private $to;
 
+    /**
+     * subject mail
+     * @var string
+     */
     private $subject;
 
+    /**
+     * message mail
+     * @var string
+     */
     private $message;
 
+    /**
+     * method constructor
+     * @param AttributesMailClass $attributes
+     * @return void
+     */
     public function __construct(AttributesMailClass $attributes)
     {
         $this->to = $attributes->to;
@@ -19,7 +40,11 @@ class ValidationMessage extends AttributesMailClass
         $this->message = $attributes->message;
     }
 
-    public function validate()
+    /**
+     * method validate
+     * @return bool
+     */
+    public function validate(): bool
     {
         if (empty($this->to) || empty($this->subject) || empty($this->message)) {
             return false;
