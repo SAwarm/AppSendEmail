b35b5b0e3b61bcbdeb626015ccb049879e85e8b7
jonas-elias <jonasdasilvaelias@gmail.com>
2022-07-21 21:48:12 -0300

    fix: add method orquestrationSendMail in ProcessMail

--- src/App/ProcessMail.php
+++ src/App/ProcessMail.php
@@ -60,32 +60,53 @@ class ProcessMail
     }
 }
 
+/**
+ * Instance of AttributesMailClass
+ * 
+ * @return object $attributes
+ */
 $attributes = new AttributesMailClass();
 
-foreach ($_POST as $key => $value) {
-    $attributes->__set($key, $value);
-}
+/**
+ * call method orchestration SendMail
+ */
+orchestrationSendMail($attributes->setAllElements($_POST, $attributes));
 
-try {
-    $validation = new ValidationMessage($attributes);
-    $processMail = new ProcessMail($attributes);
+/**
+ * Method to orchestration the process of sending mail
+ * 
+ * @param object $attributes
+ * @return mixed
+ * @throws Exception
+ */
+function orchestrationSendMail($attributes): mixed
+{
+    try {
+        $validation = new ValidationMessage($attributes);
+        $processMail = new ProcessMail($attributes);
 
-    if (!$validation->validate()) {
-        throw new Exception('Invalid Inputs!', 406);
-    }
+        if (!$validation->validate()) {
+            throw new Exception('Invalid Inputs!', 406);
+        }
 
-    $response = $processMail->send();
+        $response = $processMail->send();
 
-    if ($response) {
-        $_SESSION['success'] = true;
-        $_SESSION['message'] = 'Mail sent successfully!';
+        if ($response) {
+            $_SESSION['success'] = true;
+            $_SESSION['message'] = 'Mail sent successfully!';
+
+            header('location: /');
+
+            return true;
+        }
+
+        throw new Exception($response, 500);
+    } catch (Exception $e) {
+        $_SESSION['error'] = true;
+        $_SESSION['message'] = $e->getMessage();
+
         header('location: /');
-        return;
-    }
 
-    throw new Exception($response, 500);
-} catch (Exception $e) {
-    $_SESSION['error'] = true;
-    $_SESSION['message'] = $e->getMessage();
-    header('location: /');
+        return false;
+    }
 }
